e89900589a6f8ba5a8f88a85a3e3da303e5e6575
Mike Hayes <mikehayes330@gmail.com>
2020-08-27 22:01:49 -0400

    adding jawsdb

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+ INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+ INSERT INTO burgers (burger_name) VALUES ('Breakfast Burger');
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Tuna Burger', true);
+     
